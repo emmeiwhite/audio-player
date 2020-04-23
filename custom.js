806704559f0e479d91c9184417479efe3f032f75
emmeiwhite <emmeiwhite@gmail.com>
2020-04-23 17:24:40 +0530

    playAndPauseHandler used to toggle our play and pause Images

--- custom.js
+++ custom.js
@@ -4,14 +4,20 @@ document.addEventListener('DOMContentLoaded', function (e) {
   const playIcon = document.getElementById("play-icon");
   const pauseIcon = document.getElementById("pause-icon");
 
-  playIcon.addEventListener('click', function (e) {
-    this.style.display = "none";
-    pauseIcon.style.display = "block";
-  });
+  function playAndPauseHandler(e) {
+    if (this.id === 'play-icon') {
+      console.log('play-icon clicked');
+      playIcon.style.display = 'none';
+      pauseIcon.style.display = 'block';
+    }
 
+    if (this.id === 'pause-icon') {
+      console.log('pause-icon clicked');
+      pauseIcon.style.display = 'none';
+      playIcon.style.display = 'block';
+    }
+  }
 
-  pauseIcon.addEventListener('click', function (e) {
-    this.style.display = "none";
-    playIcon.style.display = "block";
-  });
+  playIcon.addEventListener('click', playAndPauseHandler);
+  pauseIcon.addEventListener('click', playAndPauseHandler);
 });
