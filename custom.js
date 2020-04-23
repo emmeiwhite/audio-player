a373c9f31d0b8b07d8db68875ff2b9aa948a1da6
emmeiwhite <emmeiwhite@gmail.com>
2020-04-23 20:20:31 +0530

    volume onchange handler provided to increase and decrease the volume

--- custom.js
+++ custom.js
@@ -1,39 +1,19 @@
-// document.getElementById('audio').setAttribute("controls", true);
-
 document.addEventListener('DOMContentLoaded', function (e) {
   const playIcon = document.getElementById("play-icon");
   const pauseIcon = document.getElementById("pause-icon");
-
+  const volume = document.querySelector('.volume');
 
   const audio = new Audio('./mpthreetest.mp3');
 
+
+  /** --- play pause Handler --- */
   function playAndPauseHandler(e) {
-    /**
-    if (this.id === 'play-icon') {
-      console.log('play-icon clicked');
-      playIcon.style.display = 'none';
-      pauseIcon.style.display = 'block';
-
-      audio.play();
-    }
-
-    if (this.id === 'pause-icon') {
-      console.log('pause-icon clicked');
-      pauseIcon.style.display = 'none';
-      playIcon.style.display = 'block';
-
-      audio.pause();
-    }
-     */
-
     if (audio.paused) {
-      console.log('play-icon clicked');
       playIcon.style.display = 'none';
       pauseIcon.style.display = 'block';
 
       audio.play();
     } else {
-      console.log('pause-icon clicked');
       pauseIcon.style.display = 'none';
       playIcon.style.display = 'block';
 
@@ -41,6 +21,16 @@ document.addEventListener('DOMContentLoaded', function (e) {
     }
   }
 
+
+  /** --- volume handler --- */
+  function volumeHandler(e) {
+    console.log(e.target.value);
+    audio.volume = e.target.value;
+  }
+
+
   playIcon.addEventListener('click', playAndPauseHandler);
   pauseIcon.addEventListener('click', playAndPauseHandler);
+  volume.addEventListener('change', volumeHandler);
+
 });
