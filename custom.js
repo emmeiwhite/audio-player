3492774c40ad56d85ddf0920f2b69148118f1305
emmeiwhite <emmeiwhite@gmail.com>
2020-04-23 19:21:04 +0530

    Making use of  Audio Object in Broswer Web API

--- custom.js
+++ custom.js
@@ -4,17 +4,40 @@ document.addEventListener('DOMContentLoaded', function (e) {
   const playIcon = document.getElementById("play-icon");
   const pauseIcon = document.getElementById("pause-icon");
 
+
+  const audio = new Audio('./mpthreetest.mp3');
+
   function playAndPauseHandler(e) {
+    /**
     if (this.id === 'play-icon') {
       console.log('play-icon clicked');
       playIcon.style.display = 'none';
       pauseIcon.style.display = 'block';
+
+      audio.play();
     }
 
     if (this.id === 'pause-icon') {
       console.log('pause-icon clicked');
       pauseIcon.style.display = 'none';
       playIcon.style.display = 'block';
+
+      audio.pause();
+    }
+     */
+
+    if (audio.paused) {
+      console.log('play-icon clicked');
+      playIcon.style.display = 'none';
+      pauseIcon.style.display = 'block';
+
+      audio.play();
+    } else {
+      console.log('pause-icon clicked');
+      pauseIcon.style.display = 'none';
+      playIcon.style.display = 'block';
+
+      audio.pause();
     }
   }
 
